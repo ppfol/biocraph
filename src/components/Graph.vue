e8f771cafc09535fa1eba875d0ce87b03701220b
ppfol <wtyrrell@qq.com>
2024-11-16 23:12:52 +0800

    struct 0.01

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,44 +1,37 @@
 <script setup>
 import * as d3 from "d3";
 import { ref, onMounted, watch } from 'vue';
+
+let graph = {
+  "links": [
+    { "source": "18470926", "target": "10097404", "type": "CITED_BY" }
+  ],
+  "nodes": [
+    {
+      "id": "18470926"
+    },
+    {
+      "id": "10097404"
+    }
+  ]
+};
+
 function go() {
   const width = window.innerWidth, height = window.innerHeight;
-
   const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-  const types = ["CITED_BY"]
-
-
-  let graph = {
-    "links": [
-      { "source": "18470926", "target": "10097404", "type": "CITED_BY" }
-    ],
-    "nodes": [
-      {
-        "label": "Article",
-        "id": "18470926"
-      },
-      {
-        "label": "Article",
-        "id": "10097404"
-      }
-    ]
-  };
   const simulation = d3.forceSimulation(graph.nodes)
     .force("charge", d3.forceManyBody().strength(-3000))
-    .force("x", d3.forceX(width / 2).strength(1))
-    .force("y", d3.forceY(height / 2).strength(1))
-    .force("link", d3.forceLink(graph.links).id(function (d) { return d.id; }).distance(50).strength(1))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("link", d3.forceLink(graph.links).id(function (d) { return d.id; }).distance(100).strength(1))
     .on("tick", ticked);
 
   const svg = d3.select("#graph").append("svg")
     .attr("width", width).attr("height", height)
     .attr("style", "border: 5px solid white;")
 
-  svg.append("defs").selectAll("marker")
-    .data(types)
-    .join("marker")
-    .attr("id", d => `arrow-${d}`)
+  svg.append("defs");
+  svg.select("defs").append("marker")
+    .attr("id", "arrow")
     .attr("viewBox", "0 0 10 10")
     .attr("refX", 10)
     .attr("refY", 5)
@@ -49,63 +42,57 @@ function go() {
     .attr("fill", color)
     .attr("d", 'M 0 0 L 10 5 L 0 10 z');
 
-  const link = svg.append("g")
-    .selectAll("g")
-    .data(graph.links)
-    .enter()
-    .append("line")
-    .attr("stroke", d => color(d.type))
+  const links = svg.append("g").attr("class", "links").selectAll("g")
+    .data(graph.links).enter().append("g")
+  links.append("line")
+    .attr("stroke", color)
     .attr("stroke-width", 1)
-    .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
+    .attr("marker-end", `url(${new URL(`#arrow`, location)})`);
+  links.append("text")
+    .text(function (d) {
+      return d.type;
+    }).style('fill', 'white');
 
-  const node = svg.append("g")
-    .selectAll("g")
-    .data(graph.nodes)
-    .enter().append("g");
-
-  node.append("circle")
+  const nodes = svg.append("g").attr("class", "nodes").selectAll("g")
+    .data(graph.nodes).enter().append("g")
+  nodes.append("circle")
     .attr("r", 5)
     .attr("fill", function (d) { return color(d.label); })
-
-  node.call(
-    d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended)
-  );
-  node.append("text")
+    .call(
+      d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+  nodes.append("text")
     .text(function (d) {
       return d.id;
-    })
-    .attr('x', 6)
-    .attr('y', 3).style('fill', 'white');
-
-  node.append("title")
-    .text(function (d) { return d.label; });
+    }).style('fill', 'white');
 
   function ticked() {
-
-    node.call(updateNode);
-    link.call(updateLink);
-
+    nodes.call(updateNode);
+    links.call(updateLink);
   }
 
-  function fixna(x) {
-    if (isFinite(x)) return x;
-    return 0;
+  function updateLink(links) {
+    links.selectAll("line")
+      .attr("x1", function (d) { return d.source.x; })
+      .attr("y1", function (d) { return d.source.y; })
+      .attr("x2", function (d) { return d.target.x; })
+      .attr("y2", function (d) { return d.target.y; });
+    links.selectAll("text")
+      .attr("x", function (d) {return (d.source.x+d.target.x)/2 + 5;})
+      .attr("y", function (d) {return (d.source.y+d.target.y)/2 + 5;});
   }
 
-  function updateLink(link) {
-    link.attr("x1", function (d) { return fixna(d.source.x); })
-      .attr("y1", function (d) { return fixna(d.source.y); })
-      .attr("x2", function (d) { return fixna(d.target.x); })
-      .attr("y2", function (d) { return fixna(d.target.y); });
-  }
-
-  function updateNode(node) {
-    node.attr("transform", function (d) {
-      return "translate(" + fixna(d.x) + "," + fixna(d.y) + ")";
-    });
+  function updateNode(nodes) {
+    nodes.selectAll("circle")
+      .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+    nodes.selectAll("text")
+    .attr("x", function (d) {return d.x + 5;})
+    .attr("y", function (d) {return d.y + 5;});
   }
 
   function dragstarted(event, d) {
